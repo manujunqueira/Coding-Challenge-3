74a1c31a154727c573d531ed10d0cad3f8a4a94f
manujunqueira <manuelajunqueira27@gmail.com>
2024-09-08 19:16:17 -0400

    "Develop findTopAndBottomPerformers function"

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -38,5 +38,27 @@ function determinePerformanceRating (x)
 let performanceRating = determinePerformanceRating(salesFigures);
 console.log(performanceRating)
 
+// Task 3 - Create a Function to Identify Top and Bottom Performers
 
+function findTopAndBottomPerformers (salesData) {
+    const totalSales = salesData.map(salesPerson => ({ //used map to create new array
+        name: salesPerson.name, 
+        totalSales: salesPerson.sales.reduce((sum,sale)=> sum + sale, 0)
+    }));
 
+    const maxSales = Math.max(...totalSales.map(salesPerson => salesPerson.totalSales)); 
+
+    const minSales = Math.min(...totalSales.map(salesPerson => salesPerson.totalSales));
+
+    //math.max will recognize the person with the most sales and math.min will do the opposite job, identifying the one with the least sales. 
+
+    return {
+        topPerformer: totalSales.find(salesPerson => salesPerson.totalSales === maxSales),
+        bottomPerformer: totalSales.find(salesPerson => salesPerson.totalSales === minSales)
+    };//the one with the most sales will be returned as "top performer and the one woth the least sales as "bottom performer"
+};
+
+let topBottomPerformers = findTopAndBottomPerformers(salesData); //pulling the data from the sales dataset given
+
+console.log("Top Performer:", topBottomPerformers.topPerformer);
+console.log("Bottom Performer:", topBottomPerformers.bottomPerformer); 
